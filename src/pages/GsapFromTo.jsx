c139d27cb4ef9e20b2c53c51d68d060e0b12631e
NayefNemernn <nayefnemernn@gmail.com>
2024-09-01 00:10:54 +0300

    using gsap.fromTo() to animate box from newState into newState

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap'
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+
+  useGSAP(()=>{
+    gsap.fromTo('#red-box',{
+      x:0,
+      ease: 'power1.inOut'
+
+    },{
+      x: 250,
+      repeat: -1,
+      borderRadius: '50%',
+      duration: 2,
+      yoyo: true,
+      rotation: 360,
+      ease: 'power1.inOut',
+    })
+  },[])
+
 
   return (
     <main>
